908afb47b5e368fd31e9d07458ea46434ab5e813
cunzhong <cunzhong@qq.com>
2015-11-06 01:26:38 +0800

    2015/11/06  1:26

--- 其他/txt2dsl.py
+++ 其他/txt2dsl.py
@@ -1,17 +1,18 @@
 import re
 
-srcfile = 'E:/王力古漢語字典.txt'
+srcfile = "E:/本草綱目.txt"
 srcencode = 'utf-8'
-outfile = "E:/王力古漢語字典.dsl"
-name = '王力古漢語字典'
-prefix ='WL'
-totalpg = 1791
+outfile = "E:/本草綱目.dsl"
+name = '本草綱目'
+prefix ='BC'
+totalpg = 2000
 n = len(str(totalpg))
-index = 90
-fow = 20
-att = 0
+index = 28
+fow = 18
+att = 224
+indexatt = True
 
-getzi = re.compile(r'^\S+(?=\t)')
+getzi = re.compile(r'^[^\t]+(?=\t)')
 getpg = re.compile(r'(?<=\t)\d+')
 getno = re.compile(r'(?<=\.)\d+$')
 
@@ -35,9 +36,9 @@ for line in lines:
 	pg = (int(getpg.search(line).group()))
 	if getno.search(line):
 		no = getno.search(line).group()
-		output.write(zi + '\n\t<<' + prefix + str(pg).zfill(n) + '>>之' + no + '。\n\t')
+		output.write(zi + '\n\t<<' + prefix + str(pg).zfill(n) + '>>之' + no + '\n\t')
 	else:
-		output.write(zi + '\n\t<<' + prefix + str(pg).zfill(n) + '>>。\n\t')
+		output.write(zi + '\n\t<<' + prefix + str(pg).zfill(n) + '>>\n\t')
 	pages(pg)
 
 for pg in range (1, totalpg + 1):
@@ -54,15 +55,26 @@ for pg in range (1, index + 1):
 		bf = '<<' + prefix + 'I' + str(pg-1).zfill(3) + '>> <<' + prefix + 'I' + str(pg+1).zfill(3) + '>>'
 		output.write(bf + '[s]!' + str(pg).zfill(5) + '.tif[/s]' + bf + '\n')
 
+if indexatt == True:
+	for pg in range (1, att + 1):
+		output.write(prefix + 'A' + str(pg).zfill(3) + '\n\t')
+		if pg == 1:
+			output.write('<<' + prefix + 'A' + str(pg+1).zfill(3) + '>>[s]att' + str(pg).zfill(3) + '.tif[/s]<<' + prefix + 'A' + str(pg+1).zfill(3) + '>>\n')
+		elif pg == att:
+			output.write('<<' + prefix + 'A' + str(pg-1).zfill(3) + '>>[s]att' + str(pg).zfill(3) + '.tif[/s]<<' + prefix + 'A' + str(pg-1).zfill(3) + '>>\n')
+		else:
+			bf = '<<' + prefix + 'A' + str(pg-1).zfill(3) + '>> <<' + prefix + 'A' + str(pg+1).zfill(3) + '>>'
+			output.write(bf + '[s]att' + str(pg).zfill(3) + '.tif[/s]' + bf + '\n')
+
 output.write(name + '\n\t')
-output.write('版權[s]leg001.tif[/s]\n\t')
+output.write('版權[s]leg001.tif[/s]')
 if fow > 0:
 	fows = ''
 	for pg in range (1, fow + 1):
 		fows += '[s]fow' + str(pg).zfill(3) + '.tif[/s]'
-	output.write('前言' + fows + '\n\t')
-if att > 0:
+	output.write('\n\t前言' + fows)
+if indexatt != True and att > 0:
 	atts = ''
 	for pg in range (1, att + 1):
 		atts += '[s]att' + str(pg).zfill(3) + '.tif[/s]'
-	output.write('附錄' + atts)
+	output.write('\n\t附錄' + atts)
